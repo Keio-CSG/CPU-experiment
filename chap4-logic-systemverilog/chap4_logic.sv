2a967f98eb2e50766d1df5965a835e107d8e49df
Kentaro Yoshioka <meathouse47@gmail.com>
2021-11-18 13:33:31 +0900

    SystemVerilogで書いた組み合わせロジック例を追加

---
+++ chap4-logic-systemverilog/chap4_logic.sv
@@ -0,0 +1,16 @@
+
+// adder in system verilog
+module adder4 (input logic [3:0] a,
+            input logic [3:0] b,
+            input logic cin, 
+            output logic [3:0] sum, 
+            output logic cout);
+    assign sum = a + b + cin;
+//    assign (cout, sum) = a + b + cin;
+endmodule
+
+// module in system verilog
+module inv4 (input logic [3:0] a,
+            output  logic [3:0] y);
+    assign y = ~a;
+endmodule
