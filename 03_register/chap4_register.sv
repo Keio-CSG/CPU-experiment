91519af40f40b8e1eb00e8a96611ab8454cf9cc8
Kentaro Yoshioka <meathouse47@gmail.com>
2021-11-25 10:39:18 +0900

    add flopr

--- 03_register/chap4_register.sv
+++ 03_register/chap4_register.sv
@@ -9,3 +9,16 @@ module flop(
         q <= d;
     
 endmodule
+
+module flopr(
+    input logic clk,
+    input logic rst,
+    input logic [3:0] d,
+    output logic [3:0] q
+);
+
+    always_ff @(posedge clk, posedge rst)
+        if (reset) q <= 4'b0;
+        else q <= d;
+endmodule
+
