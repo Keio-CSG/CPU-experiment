be3379d9dc379252561161acdd9b242e9a187c70
Kentaro Yoshioka <meathouse47@gmail.com>
2021-11-25 10:41:19 +0900

    add flopr with enable

--- 03_register/chap4_register.sv
+++ 03_register/chap4_register.sv
@@ -18,7 +18,19 @@ module flopr(
 );
 
     always_ff @(posedge clk, posedge rst)
-        if (reset) q <= 4'b0;
+        if (rst) q <= 4'b0;
         else q <= d;
 endmodule
 
+module flopenr(
+    input logic clk,
+    input logic rst,
+    input logic en,
+    input logic [3:0] d,
+    output logic [3:0] q
+);
+
+    always_ff @(posedge clk, posedge rst)
+        if (rst) q <= 4'b0;
+        else if (en) q <= d;
+endmodule
