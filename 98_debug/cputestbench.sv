b634a6d9ccde0dffd8d401afb73b34d24c90d1fb
Kentaro Yoshioka <meathouse47@gmail.com>
2021-11-26 16:24:06 +0900

    fix bugs

--- 98_debug/cputestbench.sv
+++ 98_debug/cputestbench.sv
@@ -5,12 +5,12 @@ module testcpu2;
        cpu u3(.*);
      
       initial begin
-      inst=32'b000000000010000010000000000000;
-      clk=0;
-      clk2 = 0;
-      #1000 $stop;   
+        inst=32'b000000000010000010000000000000;
+        clk=0;
+        clk2 = 0;
+        #1000 $stop;   
       end
-      
+
       always #10 begin
         clk=clk+1;    
         clk2 = ~clk2;
